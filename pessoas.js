f15b820c4370f70d17853c72fcfe821ec1cc0b74
pontodeluz <91508589+pontodeluz@users.noreply.github.com>
2023-11-29 14:34:08 -0300

    informações complementares

--- pessoas.js
+++ pessoas.js
@@ -1,10 +1,21 @@
 const pessoas = [];
+/**
+ * Para adicionar pessoas, é necessário saber antes os blocos, 
+ * para isso, acesse o seguinte link:
+ * https://pontodeluz.github.io/dashboard.html
+ * 
+ * Para visualizr os pontos de luz:
+ * https://pontodeluz.github.io
+ */
+
+//maximo de blocos [1224]
 pessoas.push({bloco: 574, nome: "Sandra"});
 pessoas.push({bloco: 575, nome: "Katia"});
 pessoas.push({bloco: 576, nome: "Patricia"});
 pessoas.push({bloco: 577, nome: "Pedro"});
 pessoas.push({bloco: 578, nome: "Arthur"});
 pessoas.push({bloco: 579, nome: "Zé"});
+
 
 /*
 
