c4d17145f33f37e558ee680f4c69f8769ce4662a
pontodeluz <91508589+pontodeluz@users.noreply.github.com>
2023-11-29 14:29:07 -0300

    Ajuste de dependencia

---
+++ pessoas.js
@@ -0,0 +1,17 @@
+const pessoas = [];
+pessoas.push({bloco: 574, nome: "Sandra"});
+pessoas.push({bloco: 575, nome: "Katia"});
+pessoas.push({bloco: 576, nome: "Patricia"});
+pessoas.push({bloco: 577, nome: "Pedro"});
+pessoas.push({bloco: 578, nome: "Arthur"});
+pessoas.push({bloco: 579, nome: "Zé"});
+
+/*
+
+//gerador de pessoas para teste
+let estrelinhas = 0;
+for(let i = 0; i < 1225; i++){
+    pessoas.push({bloco: i, nome: "Fulano"})
+    estrelinhas++
+}*/
+
