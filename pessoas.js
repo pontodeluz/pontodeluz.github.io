9dd03352d304499e46eda2774e46e4e6a84f6daf
pontodeluz <91508589+pontodeluz@users.noreply.github.com>
2023-12-01 11:31:32 -0300

    Update pessoas.js

--- pessoas.js
+++ pessoas.js
@@ -14,7 +14,33 @@ pessoas.push({bloco: 931, nome: "Katia"});
 pessoas.push({bloco: 576, nome: "Patricia"});
 pessoas.push({bloco: 300, nome: "Pedro"});
 pessoas.push({bloco: 578, nome: "Arthur"});
-pessoas.push({bloco: 791, nome: "Zé"});
+pessoas.push({bloco: 999, nome: "Zé"});
+pessoas.push({bloco: 1000, nome: "Zé"});
+pessoas.push({bloco: 1001, nome: "Zé"});
+pessoas.push({bloco: 964, nome: "Zé"});
+pessoas.push({bloco: 965, nome: "Zé"});
+pessoas.push({bloco: 966, nome: "Zé"});
+pessoas.push({bloco: 929, nome: "Zé"});
+pessoas.push({bloco: 930, nome: "Zé"});
+pessoas.push({bloco: 931, nome: "Zé"});
+pessoas.push({bloco: 932, nome: "Zé"});
+pessoas.push({bloco: 895, nome: "Zé"});
+pessoas.push({bloco: 896, nome: "Zé"});
+pessoas.push({bloco: 897, nome: "Zé"});
+pessoas.push({bloco: 862, nome: "Zé"});
+pessoas.push({bloco: 863, nome: "Zé"});
+pessoas.push({bloco: 827, nome: "Zé"});
+pessoas.push({bloco: 1035, nome: "Zé"});
+pessoas.push({bloco: 1036, nome: "Zé"});
+pessoas.push({bloco: 1070, nome: "Zé"});
+pessoas.push({bloco: 1071, nome: "Zé"});
+
+
+
+
+
+
+
 
 
 
